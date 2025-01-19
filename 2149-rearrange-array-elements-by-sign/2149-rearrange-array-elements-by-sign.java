cef89377375a5698cba3b743ab4ab72f5bc9613d
ItzVarad <70215793+ItzVarad@users.noreply.github.com>
2025-01-20 01:10:37 +0530

    Time: 3 ms (100%), Space: 77.3 MB (37.71%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int len = nums.length, o=0,e=0;
-        int[] odd = new int[len/2];
-        int[] even = new int[len/2];
-        for(int num:nums){
-            if(num>0) even[e++] = num;
-            else odd[o++] = num;
+        int len = nums.length, pos = 0, neg = 1;
+        int[] ans = new int[len];
+        for(int i = 0; i<len; i++){
+            if(nums[i] > 0){
+                ans[pos] = nums[i];
+                pos += 2;
+            }
+            else{
+                ans[neg] = nums[i];
+                neg += 2;
+            }
         }
-        o=0;
-        e=0;
-        for(int i = 0;i<len;i++){
-            if(i % 2 == 0) nums[i] = even[e++];
-            else nums[i] = odd[o++];
-        }
-        return nums;
+        return ans;
     }
 }
